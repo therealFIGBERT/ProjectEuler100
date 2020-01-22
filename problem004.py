b7234294975065e7906701e869e70da13fdedc74
therealFIGBERT <figbertwelner@gmail.com>
2020-01-21 16:58:05 -0800

    Simplify solution to problem 4

--- problem004.py
+++ problem004.py
@@ -4,11 +4,5 @@
 # language = "Python"
 # dateCompleted = "21/01/2020"
 
-def palindromeCheck(num):
-    reversedNum = int(str(num)[::-1])
-    if reversedNum == num:
-        return True
-    return False
-
-allPossibleCombos = [a*b for a in range(999, 99, -1) for b in range(999, 99, -1) if palindromeCheck(a*b)]
+allPossibleCombos = [a*b for a in range(999, 99, -1) for b in range(999, 99, -1) if int(str(a*b)[::-1]) == a*b]
 print("The largest palindrome made from the product of two 3-digit numbers is %s" % max(allPossibleCombos))

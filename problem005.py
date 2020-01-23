fa05097c70083a6a88df4a7eb72ffcd1e702f798
therealFIGBERT <figbertwelner@gmail.com>
2020-01-23 11:18:44 -0800

    Add solution to problem 5

---
+++ problem005.py
@@ -0,0 +1,14 @@
+# problemName = "Smallest multiple"
+# problemNum = 5
+# solutionBy = "FIGBERT"
+# language = "Python"
+# dateCompleted = "21/01/2020"
+
+answer = 1
+for i in range(1, 21):
+    if answer % i > 0:
+        for k in range(1, 21):
+            if (answer * k) % i == 0:
+                answer *= k
+                break
+print("The smallest positive number that is evenly divisible by all of the numbers from 1 to 20 is %s" % answer)

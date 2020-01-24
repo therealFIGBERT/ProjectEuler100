41457cf3cbcb98262f3b9bed42d96ee7348418b7
therealFIGBERT <figbertwelner@gmail.com>
2020-01-24 11:35:41 -0800

    Add solution to problem 6

---
+++ problem006.py
@@ -0,0 +1,14 @@
+# problemName = "Sum square difference"
+# problemNum = 6
+# solutionBy = "FIGBERT"
+# language = "Python"
+# dateCompleted = "24/01/2020"
+
+sumOfTheSquares = 0
+squareOfTheSum = 0
+for i in range(1, 101):
+    sumOfTheSquares += i**2
+    squareOfTheSum += i
+squareOfTheSum = squareOfTheSum**2
+answer = squareOfTheSum - sumOfTheSquares
+print("The difference between the sum of the squares of the first one hundred natural numbers and the square of the sum is %s" % answer)
